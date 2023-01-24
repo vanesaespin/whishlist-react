7d174b6e93512b97cba1e659053ba8a7fec9bc6e
vanesaespin <vespin@ieshlanz.es>
2023-01-24 17:44:24 +0100

    setup de clase funcionando

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const root = ReactDom.createRoot(
+  document.getElementById('root')
+);
+
+root.render(
+  <div> Wishlist de react</div>
+);
