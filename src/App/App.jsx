b0707134850e5f95155e56f8d15522dbc2f8ecc1
vanesaespin <vespin@ieshlanz.es>
2023-01-24 13:17:05 +0100

    nuevos componentes

---
+++ src/App/App.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-lone-blocks */
+import React from 'react';
+import './App.css';
+import WishInput from './WishInput';
+import WishList from './WishList';
+
+{ /* SEPARAR LA LISTA DE DESEOS EN COMPONENTES DIFERENTES */ }
+
+const deseos = [
+  { texto: 'ir a la luna', cumplido: false },
+  { texto: 'aprobar este módulo', cumplido: false },
+  { texto: 'pagar el gimnasio', cumplido: true },
+  { texto: 'aprender React', cumplido: false },
+
+];
+
+function App() {
+  return (
+    <div className="app"> 
+      <h1>My wish list APP </h1>
+      <WishInput />
+      {/* botón */}
+      <button type="button" className="deseos-clear">Archivar deseos cumplidos</button>
+    </div>
+  );
+}
+
+export default App;
